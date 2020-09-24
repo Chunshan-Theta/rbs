f2bc0e7a931470bc9b81b8d3091e879c97a34572
Gavin Mac mini <gavin19950511@gmail.com>
2020-09-24 22:09:24 +0800

    docs(env):docs

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-git clone https://github.com/julia-/room-booking-system
+#git clone https://github.com/julia-/room-booking-system
 brew install yarn
 cd room-booking-system/api
 yarn install 
 cd ../web
 yarn install
-
-yarn add seed
