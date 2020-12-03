bae8882d4e55538a6661a76dd37b71f3d2118eda
gavinwang <gavin.wang@silkrode.com.tw>
2020-12-03 11:43:18 +0800

    feat(create room): add feature

--- room-booking-system/web/src/components/NavBar.js
+++ room-booking-system/web/src/components/NavBar.js
@@ -9,9 +9,10 @@ function NavBar({
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="nav__item"><Link to="/bookings" className="nav__link">View Room Availability</Link></li>
-        <li className="nav__item"><Link to="/mybookings" className="nav__link">My Bookings</Link></li>
-        <li className="nav__item"><a onClick={signOut} className="nav__link">Logout</a></li>
+        <li className="nav__item"><Link to="/bookings" className="nav__link">主頁</Link></li>
+        <li className="nav__item"><Link to="/mybookings" className="nav__link">我的預定</Link></li>
+        <li className="nav__item"><Link to="/createroom" className="nav__link">建立新空間</Link></li>
+        <li className="nav__item"><a onClick={signOut} className="nav__link">登出</a></li>
       </ul>
     </nav>
   )
